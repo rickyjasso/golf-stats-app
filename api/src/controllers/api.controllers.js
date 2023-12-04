f507b86161fd8baf8e93927cb908dc07467a499c
rickyjasso <rickyjassoa@gmail.com>
2023-12-04 11:48:25 -0600

    backend endpoints for inserting and getting clubs

--- api/src/controllers/api.controllers.js
+++ api/src/controllers/api.controllers.js
@@ -3,6 +3,67 @@ const jwt = require('jsonwebtoken');
 const {SECRET} = require('../constants')
 
 
+exports.addGolfClub = async(req, res) => {
+    let token = req.cookies.token;
+    if (!token) {
+        // Handle the case when there's no token (user not authenticated)
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const {club_type, club_number} = req.body
+    try {
+        let decoded = jwt.verify(token, SECRET);
+        let golf_player = decoded.id;
+        await db.query(`INSERT INTO golf_club (golf_bag_id, club_type, club_number)
+                        VALUES ((SELECT id FROM golf_bag WHERE player_id = $1), $2, $3)`, [golf_player, club_type, club_number])
+        return res.status(200).json({
+            success: true,
+            message: 'Golf club added successfully!'
+        })
+        } catch (error) {
+            res.status(500).json({
+                error: error.message
+            })
+        }
+
+}
+
+exports.getGolfClubs = async(req, res) => {
+    let token = req.cookies.token;
+    if (!token) {
+        // Handle the case when there's no token (user not authenticated)
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+        let decoded = jwt.verify(token, SECRET);
+        let golf_player = decoded.id
+
+        const gc = await db.query(`SELECT club_type, club_number 
+                                   FROM golf_player AS gp
+                                   JOIN golf_bag AS gb ON gp.id = gb.player_id
+                                   JOIN golf_club AS gc ON gb.id = gc.golf_bag_id
+                                   WHERE gp.id = $1;`, [golf_player])
+
+        if (gc.rows.length === 0) {
+            throw new Error('No golf clubs.')
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: 'Golf clubs fetched correctly.',
+            golf_clubs: gc.rows,
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+
+    return
+}
+
+
 exports.createGolfBag = async(req, res) => {
     let token = req.cookies.token;
     if (!token) {
--- api/src/routes/api.routes.js
+++ api/src/routes/api.routes.js
@@ -1,8 +1,10 @@
-const { getGolfBag, createGolfBag } = require('../controllers/api.controllers');
+const { getGolfBag, createGolfBag, addGolfClub, getGolfClubs } = require('../controllers/api.controllers');
 const {Router} = require('express');
 const router = Router();
 
     router.get('/golfbag', getGolfBag);
     router.post('/golfbag/new', createGolfBag);
+    router.post('/golfclubs/new', addGolfClub);
+    router.get('/golfclubs', getGolfClubs);
 
 module.exports = router
